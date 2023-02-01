c974cb5f860f98a4488989ea3c8c1d7d32922c99
Nicolás Felipe Dipierro <82405119+FeliDipi@users.noreply.github.com>
2023-02-01 16:12:47 -0300

    *app.js (input behavior), html (input added)

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
+const $UnityInstance = document.getElementById("unity-canvas");
+const $input = document.getElementById("input-capture");
+$input.onchange = function(e){
+    const [file] = $input.files;
+    if (file) {
+        var captureURL = URL.createObjectURL(file);
+        UnityInstance.SendMessage('BrowserHook', 'SetCaptureURL', captureURL);
+    }
+  }
+
+const $img = document.getElementById("avatar-image");
+
 function LoadImage(key) {
 
-    window.alert(key);
-    console.log(key);
+    console.log("Avatar Capture key: " + key);
 
     // IndexedDB
     var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB,
         IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.OIDBTransaction || window.msIDBTransaction,
-        dbVersion = 21;
+        dbVersion = 21; //version of database
 
     let db;
-    // Create/open database
+    // open database
     var request = indexedDB.open("/idbfs", dbVersion)
     request.onsuccess = function(e){
 
-        console.log("Success creating/accessing IndexedDB database");
+        console.log("Success accessing IndexedDB database");
 
-        db = request.result;
+        db = request.result;//save database hash
         var transaction = db.transaction(["FILE_DATA"], IDBTransaction.READ_WRITE);
         transaction.objectStore("FILE_DATA").get(key).onsuccess = function (event) {
-            var imgFile = event.target.result;
-            console.log(imgFile);
-
-            var blob = new Blob([imgFile.contents], { type: "image/png" });
+            var imgFile = event.target.result;//get file object of avatar capture
+            var blob = new Blob([imgFile.contents], { type: "image/png" });//format file object to image object to convert as URL link
 
             // Create and revoke ObjectURL
             var imgURL = (window.URL) ? window.URL.createObjectURL(blob) : window.webkitURL.createObjectURL(blob);
 
             // Set img src to ObjectURL
-            var img = document.getElementById("display_image");
-            img.setAttribute("src", imgURL);
+            $img.setAttribute("src", imgURL);
         };
     }
 
     request.onerror = function (event) {
-        console.log("Error creating/accessing IndexedDB database");
+        console.log("Error accessing IndexedDB database");
     };
-  }
+}
